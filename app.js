3ba4c51b8b10d0d314e9e19b6f1212ca5def6ff4
polla3 <161010690+polla3@users.noreply.github.com>
2024-04-16 14:45:42 -0700

    Update app.js

--- app.js
+++ app.js
@@ -67,7 +67,7 @@ document.getElementById('form').addEventListener('submit', function (event) {
     };
 
     // Send the form data via Email.js
-    emailjs.send("service_cczcb0i", "template_k7ixms9", formData)
+    emailjs.send("service_1h1ea3s", "template_r3pnabf", formData)
         .then(function (response) {
             console.log('SUCCESS!', response.status, response.text);
             redirectToURL();
